a60272c11cf395fa482350b049369785ee3defcb
camilla.falcao <millafalcao.acad@gmail.com>
2022-07-06 13:43:33 -0300

    trabalhando com parcelas

--- financiamento.js
+++ financiamento.js
@@ -8,6 +8,31 @@ class Financiamento{
         this.#taxaJuros = taxaJuros;
         this.#prazo = prazo;
         //composiçao - financiamento possui ou tem parcelas
-        this.#parcelas.push(new Parcela(0,0,0,0, valor - entrada))
+        this.#parcelas.push(new Parcela(0,0,0,0, valor - entrada)) //a primeira parcela se preocupa apenas com o parametro saldo (saldo devedor, valor que a pessoa quer financiar - valor de entrada)
+    }
+
+    static calcJuros(valor, taxaJuros){
+        return valor * (taxaJuros/100);
+    }
+
+    calcParcelasMensais(){
+        let saldo = this.#parcelas[this.#parcelas.length - 1].getSaldo();
+        let prazo = this.#prazo - (this.#parcelas.length - 1);
+        let amortizacao = saldo/prazo;
+        for (let i = 0; i < prazo; i++){
+            const numero = this.#parcelas.length;
+            const juros = Financiamento.calcJuros(saldo, this.#taxaJuros);
+            const valor = juros + amortizacao;
+            saldo -= amortizacao;
+            if (saldo < 0){saldo = 0};
+            this.#parcelas.push(new Parcela(numero, valor, juros, amortizacao, saldo))
+        }
+    }
+
+    exibeParcelas(){
+        const parcelas = this.#parcelas.slice(1);
+        for(const parcela of parcelas){
+            
+        }
     }
 }
--- parcela.js
+++ parcela.js
@@ -11,4 +11,17 @@ export class Parcela{
         this.#saldo = saldo;
         this.#valor = valor;
     }
+
+    getSaldo(){
+        return this.#saldo;
+    }
+    getDadosFormatados(){
+        const dados = [];
+        dados.push(this.#numero);
+        dados.push(this.#valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        dados.push(this.#juros.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        dados.push(this.#amortizacao.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        dados.push(this.#saldo.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+        return dados;
+    }
 }
